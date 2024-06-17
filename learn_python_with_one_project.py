f074f330e17b3fc93f9c1d67766f75a61536c569
victorcynzo <vynzonides@gmail.com>
2024-06-17 14:40:03 +0200

    Deposit Function

--- learn_python_with_one_project.py
+++ learn_python_with_one_project.py
@@ -1,1 +1,16 @@
 #text based slot machine
+
+#starting point, collecting user input
+def deposit(): #function for collecting user input
+    while True: #continually ask until valid, need while loop
+        amount = input("What would you like to deposit? $")
+        #check if amount is number
+        if amount.isdigit(): #works with whole positive numbers
+            amount = int(amount) #convert string to integer
+            if amount > 0:
+                break #out of while loop, when amount bigger than 0
+            else:
+                print("Amount must be greater than 0.") #when amount is smaller than 0
+        else:
+            print ("Please enter a number") #when text is entered
+    return amount #return amount after function is called
