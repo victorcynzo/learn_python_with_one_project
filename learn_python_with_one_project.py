8608d4e60c9234a6d2f1191fb918a3bd4e532202
victorcynzo <vynzonides@gmail.com>
2024-06-17 14:49:26 +0200

    Clarified Print Text

--- learn_python_with_one_project.py
+++ learn_python_with_one_project.py
@@ -21,13 +21,13 @@ def deposit(): #function for collecting user input
 #collect bet from the user
 def get_number_of_lines():
     while True:
-        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?") #add variable to string input
+        lines = input("Enter the number of lines to bet on (1 and " + str(MAX_LINES) + ")?") #add variable to string input
         if lines.isdigit(): #check if it's anumber
             lines = int(lines) #convert to lines
             if 1 <= lines <= MAX_LINES: #number of lines between 1 and 3
                 break
             else:
-                print("Amount must be between (1-" + str(MAX_LINES) + ")")
+                print("Amount must be between 1 and " + str(MAX_LINES) + "")
         else:
             print("Please enter a number")
     return lines #return after function is 'completed'
